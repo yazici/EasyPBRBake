bf6ef9a785820073218d9de2f9b240e4d6bf3979
RafaelMG <rafaelmtzg@gmail.com>
2019-01-19 14:36:06 -0600

    Update non-saved files

--- __init__.py
+++ __init__.py
@@ -9,11 +9,292 @@ bl_info = {
 }
 
 import bpy
-from . epbrb_main_panel import EPBRB_PT_main_panel
+from bpy.types import Panel
+from bpy.props import BoolProperty, StringProperty, IntProperty, FloatVectorProperty, FloatProperty
 
-classes = ( EPBRB_PT_main_panel )
+from . easy_pbr_bake import EasyPBRBake
 
-register, unregister = bpy.utils.register_classes_factory(classes)
+def update_autonames(self, context):
+    scn = context.scene
+    
+    if scn.epbrb_autonames:
+        name = bpy.path.basename(bpy.context.blend_data.filepath).split('.')[0]
+        scn.epbrb_base_name = name
+        scn.epbrb_albedo_name = name + '_albedo'
+        scn.epbrb_metallic_name = name + '_metallic'
+        scn.epbrb_roughness_name = name + '_roughness'
+
+bpy.types.Scene.epbrb_autonames = BoolProperty(
+    name = "Automatic Names", 
+    description = "Automatically generate the names for the textures",
+    default = True,
+    update = update_autonames
+)
+
+bpy.types.Scene.epbrb_path = StringProperty(
+    name = "Directory Path",
+    description = "Directory where all the textures will be saved",
+    default = bpy.context.blend_data.filepath,
+    subtype = 'DIR_PATH'
+)
+
+bpy.types.Scene.epbrb_base_name = StringProperty(
+    name = "Base Name",
+    description = "Base name for the textures when using automatic names",
+    default = bpy.path.basename(bpy.context.blend_data.filepath).split('.')[0]
+)
+
+bpy.types.Scene.epbrb_x_res = IntProperty(
+     name = "Resolution X",
+     description = "Texture resolution in horizontal axis",
+     default = 1024,
+     subtype = 'PIXEL'
+)
+
+bpy.types.Scene.epbrb_y_res = IntProperty(
+     name = "Resolution Y",
+     description = "Texture resolution in vertical axis",
+     default = 1024,
+     subtype = 'PIXEL'
+)
+
+bpy.types.Scene.epbrb_margin = IntProperty(
+     name = "Margin",
+     description = "Extends the baked result as a post process filter",
+     default = 16,
+     subtype = 'PIXEL'
+)
+
+bpy.types.Scene.epbrb_selected_to_active = BoolProperty(
+    name = 'Selected to Active',
+    description = 'Bake shading on the surface of selected objects to the active object',
+    default = False
+)
+
+bpy.types.Scene.epbrb_use_cage = BoolProperty(
+    name = 'Cage', 
+    description = 'Cast rays to active object from a cage',
+    default = False
+)
+
+bpy.types.Scene.epbrb_cage_extrusion = FloatProperty(
+    name = 'Extrusion',
+    description = 'Distance to use for the inward ray cast when using selected to active',
+    default = 0.0,
+    min = 0.0,
+    max = 1.0
+)
+
+bpy.types.Scene.epbrb_cage_object = StringProperty(
+    name = "Cage Object",
+    description = "Object to use as cage instead of calculating the cage from the active object with cage extrusion",
+    default = ""
+)
+
+########### Albedo #######################
+bpy.types.Scene.epbrb_enable_albedo = BoolProperty(
+    name = 'Enable Albedo', 
+    description = 'Generate albedo texture',
+    default = False
+)
+
+bpy.types.Scene.epbrb_albedo_name = StringProperty(
+    name = "Texture Name",
+    description = "Name for the albedo texture file",
+    default = bpy.path.basename(bpy.context.blend_data.filepath).split('.')[0] + '_albedo'
+)
+
+bpy.types.Scene.epbrb_albedo_clear = BoolProperty(
+    name = "Clear Image", 
+    description = "Clear albedo image before baking",
+    default = True
+)
+
+bpy.types.Scene.epbrb_albedo_clear_color = FloatVectorProperty(
+    name = 'Clear Color', 
+    description = 'Albedo clear color',
+    size = 4,
+    min = 0.0,
+    max = 1.0,
+    default = (0.0, 0.0, 0.0, 1.0),
+    subtype = 'COLOR_GAMMA'
+)
+
+########### Metallic #######################
+bpy.types.Scene.epbrb_enable_metallic = BoolProperty(
+    name = 'Enable Metallic', 
+    description = 'Generate metallic texture',
+    default = False
+)
+
+bpy.types.Scene.epbrb_metallic_name = StringProperty(
+    name = "Texture Name",
+    description = "Name for the metallic texture file",
+    default = bpy.path.basename(bpy.context.blend_data.filepath).split('.')[0] + '_metallic'
+)
+
+bpy.types.Scene.epbrb_metallic_clear = BoolProperty(
+    name = "Clear Image", 
+    description = "Clear metallic image before baking",
+    default = True
+)
+
+bpy.types.Scene.epbrb_metallic_clear_color = FloatVectorProperty(
+    name = 'Clear Color', 
+    description = 'Metallic clear color',
+    size = 4,
+    min = 0.0,
+    max = 1.0,
+    default = (0.0, 0.0, 0.0, 1.0),
+    subtype = 'COLOR'
+)
+
+########### Roughness #######################
+bpy.types.Scene.epbrb_enable_roughness = BoolProperty(
+    name = 'Enable Roughness', 
+    description = 'Generate roughness texture',
+    default = False
+)
+
+bpy.types.Scene.epbrb_roughness_name = StringProperty(
+    name = "Texture Name",
+    description = "Name for the roughness texture file",
+    default = bpy.path.basename(bpy.context.blend_data.filepath).split('.')[0] + '_roughness'
+)
+
+bpy.types.Scene.epbrb_roughness_clear = BoolProperty(
+    name = "Clear Image", 
+    description = "Clear roughness image before baking",
+    default = True
+)
+
+bpy.types.Scene.epbrb_roughness_clear_color = FloatVectorProperty(
+    name = 'Clear Color', 
+    description = 'Roughness clear color',
+    size = 4,
+    min = 0.0,
+    max = 1.0,
+    default = (0.0, 0.0, 0.0, 1.0),
+    subtype = 'COLOR'
+)
+
+class EPBRB_PT_main_panel(Panel):
+    bl_label = 'Easy PBR Bake'
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = 'render'
+    bl_idname  = 'EPBRB_PT_main_panel'
+    bl_options = {'DEFAULT_CLOSED'}
+
+    @classmethod
+    def poll(self, context):
+        return context.engine == 'CYCLES'
+    
+    def draw(self, context):
+        layout = self.layout
+        scn = context.scene
+        
+        layout.use_property_split = True
+        layout.use_property_decorate = False
+        
+        layout.prop(scn, 'epbrb_autonames')
+        layout.prop(scn, 'epbrb_path')
+        
+        row = layout.row()
+        row.prop(scn, 'epbrb_base_name')
+        if scn.epbrb_autonames:
+            row.active = False
+            row.enabled = False
+        else:
+            row.active = True
+            row.enabled = True
+
+        layout.prop(scn, 'epbrb_x_res')
+        layout.prop(scn, 'epbrb_y_res')
+        
+        layout.prop(scn, 'epbrb_margin')
+        layout.prop(scn, 'epbrb_selected_to_active')
+        row = layout.row()
+        row.prop(scn, 'epbrb_cage_extrusion', text = 'Ray Distance')
+        if scn.epbrb_selected_to_active:
+            row.active = True
+            row.enabled = True
+        else:
+            row.active = False
+            row.enabled = False
+        
+        layout.operator('easy_pbr.bake')
+        
+        layout.prop(scn, 'epbrb_enable_albedo')
+        row = layout.row()
+        row.prop(scn, 'epbrb_albedo_name')
+        if scn.epbrb_autonames:
+            row.active = False
+            row.enabled = False
+        else:
+            row.active = True
+            row.enabled = True
+        layout.prop(scn, 'epbrb_albedo_clear')
+        if scn.epbrb_albedo_clear:
+            layout.prop(scn, 'epbrb_albedo_clear_color')
+            
+        layout.prop(scn, 'epbrb_enable_metallic')
+        row = layout.row()
+        row.prop(scn, 'epbrb_metallic_name')
+        if scn.epbrb_autonames:
+            row.active = False
+            row.enabled = False
+        else:
+            row.active = True
+            row.enabled = True
+        layout.prop(scn, 'epbrb_metallic_clear')
+        if scn.epbrb_roughness_clear:
+            layout.prop(scn, 'epbrb_metallic_clear_color')
+        
+        layout.prop(scn, 'epbrb_enable_roughness')
+        row = layout.row()
+        row.prop(scn, 'epbrb_roughness_name')
+        if scn.epbrb_autonames:
+            row.active = False
+            row.enabled = False
+        else:
+            row.active = True
+            row.enabled = True
+        layout.prop(scn, 'epbrb_roughness_clear')
+        if scn.epbrb_roughness_clear:
+            layout.prop(scn, 'epbrb_roughness_clear_color')
+
+class EPBRB_PT_albedo_section(Panel):
+    bl_label = 'Albedo'
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = 'render'
+    bl_idname  = 'EPBRB_PT_albedo_section'
+    bl_parent_id = 'EPBRB_PT_main_panel'
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw(self, context):
+        layout = self.layout
+        
+    def draw_header(self, context):
+        layout = self.layout
+        scn = context.scene
+        layout.prop(scn, 'epbrb_enable_albedo', text="")
+        
+classes = (
+    EasyPBRBake,
+    EPBRB_PT_main_panel,
+)
+
+def register():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+
+def unregister():
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
 
 if __name__ == "__main__":
     register()
--- easy_pbr_bake.py
+++ easy_pbr_bake.py
@@ -1,10 +1,227 @@
 import bpy
 from bpy.types import Operator
 
+def file_exists(file_path):
+    try:
+        file = open(file_path,'r')
+        file.close()
+        return True
+    except:
+        return False
+
+def ext_file_name(file_name, ext):
+    parts = file_name.split('.')
+    if len(parts) > 1:
+        if parts[len(parts) - 1] != ext:
+            return file_name + '.' + ext
+        else:
+            return file_name
+    else:
+        return file_name + '.' + ext
+
+def get_shader_nodes(mat_slots):
+    shaders = []
+    for ms in mat_slots:
+        nodes = ms.material.node_tree.nodes
+        for node in nodes:
+            if node.bl_idname == 'ShaderNodeOutputMaterial':
+                links = node.inputs['Surface'].links
+                if links[0].from_node.bl_idname != 'ShaderNodeBsdfPrincipled':
+                    return []
+                shaders.append(links[0].from_node.name)
+    return shaders
+    
+
 class EasyPBRBake(Operator):
-    bl_idname = "easy_pbr_bake"
+    bl_idname = "easy_pbr.bake"
     bl_label = "Bake Textures"
     
     def execute(self, context):
-        print('Hola addon')
+        scn = context.scene
+        obj = context.active_object
+                
+        # Get principled shader's nodes names        
+        shaders = get_shader_nodes(obj.material_slots)
+        if shaders == []:
+            self.report({'ERROR'}, 'Incorrect materials setup')
+            return {'FINISHED', 'CANCELLED'}
+        
+        # Bake albedo
+        if scn.epbrb_enable_albedo:
+            print('Baking albedo texture')
+            
+            # Get image to bake to
+            name = scn.epbrb_albedo_name
+            file_path = ext_file_name(scn.epbrb_path + name, 'png')
+            image = None
+            if scn.epbrb_albedo_clear or not file_exists(file_path):
+                image = bpy.data.images.new(name, scn.epbrb_x_res, scn.epbrb_y_res, alpha = False)
+                image.generated_color = scn.epbrb_albedo_clear_color
+                image.colorspace_settings.name = 'sRGB'
+                image.file_format = 'PNG'
+                image.filepath_raw = file_path
+            else:
+                image = bpy.data.images.load(file_path, check_existing = False)
+                            
+            # Copy and change materials
+            mat_dict = {}
+            for ms in obj.material_slots:
+                or_mat = ms.material
+                mat = or_mat.copy()
+                ms.material = mat
+                mat_dict[ms.name] = or_mat
+            
+            # Set active texture of materials
+            for ms in obj.material_slots:
+                nodes = ms.material.node_tree.nodes
+                tex_node = nodes.new('ShaderNodeTexImage')
+                tex_node.image = image
+                ms.material.node_tree.nodes.active = tex_node
+
+            # Bake
+            bpy.ops.object.bake(type = 'DIFFUSE', width = scn.epbrb_x_res, height = scn.epbrb_y_res, pass_filter = {'COLOR'},
+                                use_clear = False, margin = scn.epbrb_margin, use_selected_to_active = scn.epbrb_selected_to_active,
+                                cage_extrusion = scn.epbrb_cage_extrusion, cage_object = scn.epbrb_cage_object)
+            
+            # Restore materials
+            for ms in obj.material_slots:
+                mat = ms.material
+                ms.material = mat_dict[ms.name]
+                bpy.data.materials.remove(mat, do_unlink = True, do_id_user = True, do_ui_user = True)
+
+            # Save and clear image
+            image.save()
+            bpy.data.images.remove(image, do_unlink = True, do_id_user = True, do_ui_user = True)
+        
+        # Bake metallic
+        if scn.epbrb_enable_metallic:
+            print('Baking metallic texture')
+            
+            # Get image to bake to
+            name = scn.epbrb_metallic_name
+            file_path = ext_file_name(scn.epbrb_path + name, 'png')
+            image = None
+            if scn.epbrb_metallic_clear or not file_exists(file_path):
+                image = bpy.data.images.new(name, scn.epbrb_x_res, scn.epbrb_y_res, alpha = False)
+                image.generated_color = scn.epbrb_metallic_clear_color
+                image.colorspace_settings.name = 'Linear'
+                image.file_format = 'PNG'
+                image.filepath_raw = file_path
+            else:
+                image = bpy.data.images.load(file_path, check_existing = False)
+                            
+            # Copy and change materials
+            mat_dict = {}
+            for ms in obj.material_slots:
+                or_mat = ms.material
+                mat = or_mat.copy()
+                ms.material = mat
+                mat_dict[ms.name] = or_mat
+            
+            # Set active texture of materials
+            for ms in obj.material_slots:
+                nodes = ms.material.node_tree.nodes
+                tex_node = nodes.new('ShaderNodeTexImage')
+                tex_node.image = image
+                ms.material.node_tree.nodes.active = tex_node
+            
+            # Connect metallic channel
+            nslot = 0
+            for ms in obj.material_slots:
+                nodes = ms.material.node_tree.nodes
+                shader = nodes[shaders[nslot]]
+                roughness_input = None
+                if shader.inputs['Metallic'].is_linked:
+                    roughness_input = shader.inputs['Metallic'].links[0].from_socket
+                else:
+                    rgb_node = ms.material.node_tree.nodes.new('ShaderNodeRGB')
+                    roughness_input = rgb_node.outputs['Color']
+                    r_val = shader.inputs['Metallic'].default_value
+                    roughness_input.default_value = (r_val, r_val, r_val, 1)
+                base_color = shader.inputs['Base Color']
+                ms.material.node_tree.links.new(roughness_input, base_color, verify_limits = True)
+                    
+                nslot += 1
+
+            # Bake
+            bpy.ops.object.bake(type = 'DIFFUSE', width = scn.epbrb_x_res, height = scn.epbrb_y_res, pass_filter = {'COLOR'},
+                                use_clear = False, margin = scn.epbrb_margin, use_selected_to_active = scn.epbrb_selected_to_active,
+                                cage_extrusion = scn.epbrb_cage_extrusion, cage_object = scn.epbrb_cage_object)
+            
+            # Restore materials
+            for ms in obj.material_slots:
+                mat = ms.material
+                ms.material = mat_dict[ms.name]
+                bpy.data.materials.remove(mat, do_unlink = True, do_id_user = True, do_ui_user = True)
+
+            # Save and clear image
+            image.save()
+            bpy.data.images.remove(image, do_unlink = True, do_id_user = True, do_ui_user = True)
+        
+        # Bake roughness
+        if scn.epbrb_enable_roughness:
+            print('Baking roughness texture')
+            
+            # Get image to bake to
+            name = scn.epbrb_roughness_name
+            file_path = ext_file_name(scn.epbrb_path + name, 'png')
+            image = None
+            if scn.epbrb_roughness_clear or not file_exists(file_path):
+                image = bpy.data.images.new(name, scn.epbrb_x_res, scn.epbrb_y_res, alpha = False)
+                image.generated_color = scn.epbrb_roughness_clear_color
+                image.colorspace_settings.name = 'Linear'
+                image.file_format = 'PNG'
+                image.filepath_raw = file_path
+            else:
+                image = bpy.data.images.load(file_path, check_existing = False)
+                            
+            # Copy and change materials
+            mat_dict = {}
+            for ms in obj.material_slots:
+                or_mat = ms.material
+                mat = or_mat.copy()
+                ms.material = mat
+                mat_dict[ms.name] = or_mat
+            
+            # Set active texture of materials
+            for ms in obj.material_slots:
+                nodes = ms.material.node_tree.nodes
+                tex_node = nodes.new('ShaderNodeTexImage')
+                tex_node.image = image
+                ms.material.node_tree.nodes.active = tex_node
+            
+            # Connect roughness channel
+            nslot = 0
+            for ms in obj.material_slots:
+                nodes = ms.material.node_tree.nodes
+                shader = nodes[shaders[nslot]]
+                roughness_input = None
+                if shader.inputs['Roughness'].is_linked:
+                    roughness_input = shader.inputs['Roughness'].links[0].from_socket
+                else:
+                    rgb_node = ms.material.node_tree.nodes.new('ShaderNodeRGB')
+                    roughness_input = rgb_node.outputs['Color']
+                    r_val = shader.inputs['Roughness'].default_value
+                    roughness_input.default_value = (r_val, r_val, r_val, 1)
+                base_color = shader.inputs['Base Color']
+                ms.material.node_tree.links.new(roughness_input, base_color, verify_limits = True)
+                    
+                nslot += 1
+
+            # Bake
+            bpy.ops.object.bake(type = 'DIFFUSE', width = scn.epbrb_x_res, height = scn.epbrb_y_res, pass_filter = {'COLOR'},
+                                use_clear = False, margin = scn.epbrb_margin, use_selected_to_active = scn.epbrb_selected_to_active,
+                                cage_extrusion = scn.epbrb_cage_extrusion, cage_object = scn.epbrb_cage_object)
+            
+            # Restore materials
+            for ms in obj.material_slots:
+                mat = ms.material
+                ms.material = mat_dict[ms.name]
+                bpy.data.materials.remove(mat, do_unlink = True, do_id_user = True, do_ui_user = True)
+
+            # Save and clear image
+            image.save()
+            bpy.data.images.remove(image, do_unlink = True, do_id_user = True, do_ui_user = True)
+
+        
         return {'FINISHED'}
